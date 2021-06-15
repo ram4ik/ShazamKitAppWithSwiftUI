1f50205df195cf6d606cc7f0e49e4678f9ed4a19
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-15 09:50:00 +0300

    Initial Commit

---
+++ ShazamKitAppWithSwiftUI/ShazamKitAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamKitAppWithSwiftUIApp.swift
+//  ShazamKitAppWithSwiftUI
+//
+//  Created by ramil on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShazamKitAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
